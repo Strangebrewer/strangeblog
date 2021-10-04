f1236ca0ff30876518fd9a283755486e3667590f
Strangebrewer <BKAShambala@gmail.com>
2021-10-04 01:03:03 -0600

    setting things up for production deploy

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,6 +6,7 @@ import categoryRoutes from './category';
 import postRoutes from './post';
 import sourceRoutes from './source';
 import userRoutes from './user';
+import { lstat } from 'fs';
 
 router.use('/blogs', blogRoutes);
 router.use('/categories', categoryRoutes);
@@ -13,8 +14,10 @@ router.use('/posts', postRoutes);
 router.use('/sources', sourceRoutes);
 router.use('/users', userRoutes);
 
-router.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+  router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+  });
+}
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -9,11 +9,19 @@ app.use(express.json({ limit: '16mb' }));
 app.use(express.text());
 
 app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    res.header('Access-Control-Allow-Origin', '***REMOVED***');
+  } else {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
-    res.header('Access-Control-Allow-Methods', '*');
-    next();
+  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'))
+}
 
 app.use(routes)
 
